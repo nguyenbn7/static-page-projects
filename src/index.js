6d81123ad44da3951cae035f9c93a4707b06e082
nguyenbn7 <nguyenbn.email@gmail.com>
2024-06-18 07:33:02 +0700

    redirect index page to portfolio

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('Hello World');
+import loaderSVG from './loader.svg';
+const bodyEle = document.querySelector('body');
+const imageTag = document.createElement('img');
+
+imageTag.src = loaderSVG;
+bodyEle?.appendChild(imageTag);
+location.replace(import.meta.env.VITE_REDIRECT_HOME_URL);
